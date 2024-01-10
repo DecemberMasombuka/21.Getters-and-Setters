0b39f7e128871ccdd72c9b777a0c4f78482d5c05
A252422 <A252422@LSBSA-19Y5HW3>
2024-01-10 12:55:25 +0200

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Getters_and_Setters
+{
+    internal class Movie
+    {
+        public string title;
+        public string director;
+        public string rating;
+
+
+        //Constructor
+        public Movie(string title, string director, string arating)
+        {
+            this.title = title;
+            this.director = director;
+            Rating = arating;
+        }
+
+        public String Rating
+        {
+            get { return rating; } //Getter
+            set //Setter
+            {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R")
+                {
+                    rating = value;
+
+                }else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Getters_and_Setters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Danger", "Mark Teryy", "PG");
+            Movie movie2 = new Movie("Sweet Love", "Bobby Gill", " ");
+
+            Console.WriteLine(movie1.Rating);
+            Console.WriteLine(movie2.Rating);
+
+            Console.Read();
+        }
+    }
+}
